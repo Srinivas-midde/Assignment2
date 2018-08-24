17661afa27ebf9f1f3fab1383068fc7bba5a193a
Srinivas-midde <sri.midde36@gmail.com>
2018-08-24 17:45:01 +0530

    Add files via upload

---
+++ src/com/capgemini/day2/NumberSquare.java
@@ -0,0 +1,13 @@
+package com.capgemini.day2;
+
+public class NumberSquare {
+
+	public static int square(int input) {
+		int n = input*1;
+		int result = input*input;
+		System.out.println("output:" + input + "*1=" + n + ",Square:" + result);
+		// TODO Auto-generated method stub
+		return result;
+	}
+
+}
